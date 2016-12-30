b369217a94c84649820842f02b45d4ec67913555
Sarah-Tosun <sarah-tosun@web.de>
2016-12-30 11:14:06 +0100

    Tabs müssten funktionieren

---
+++ app/src/main/java/de/hs_weingarten/haplaner/AufgabenFragment.java
@@ -0,0 +1,20 @@
+package de.hs_weingarten.haplaner;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class AufgabenFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        return inflater.inflate(R.layout.fragment_aufgaben, container, false);
+    }
+}
